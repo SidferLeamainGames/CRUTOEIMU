cad03c074bf53cb52e4b97aecf350ac9cb5c510d
SidferLeamainGames <158565479+SidferLeamainGames@users.noreply.github.com>
2024-03-07 17:54:40 +0300

    ИГра

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public float Speed;
+    public Transform FireBallTransform;
+    public float lifeTime;
+    public float damage = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyFireball", lifeTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+    private void MoveFixedUpdate()
+    {
+        FireBallTransform.position += transform.forward * Speed * Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DestroyFireball();
+        DamageEnemy(collision);
+    }
+
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+
+    private void DamageEnemy(Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.DealDamage(damage);
+        }
+
+        DestroyFireball();
+    }
+}
---
+++ Assets/Scripts/FireBallCaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallCaster : MonoBehaviour
+{
+    public FireBall fireBallPrefab;
+    public Transform fireballSourceTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireBallPrefab, fireballSourceTransform.position, fireballSourceTransform.rotation);
+        }
+    }
+}
